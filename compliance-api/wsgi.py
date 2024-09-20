2f946dae20e77a3a7d8e0db77d8d40868c5c5204
saravanpa-aot <Saravan.Azhikoden@gov.bc.ca>
2024-09-19 23:39:50 -0700

    Update wsgi.py

--- compliance-api/wsgi.py
+++ compliance-api/wsgi.py
@@ -1,6 +1,22 @@
 from compliance_api import create_app
 
+print("App Environment Variables----------1----------:")
+for k, v in os.environ.items():
+    print(f"{k}: {v}")
+
 application = create_app()
 
+
+print("App Environment Variables:-----------2--------")
+for k, v in os.environ.items():
+    print(f"{k}: {v}")
+
+
 if __name__ == "__main__":
+
+    print("App Environment Variables:---------3-------------")
+    for k, v in os.environ.items():
+    print(f"{k}: {v}")
+
+    
     application.run(debug=True, host='0.0.0.0', port=3200)
