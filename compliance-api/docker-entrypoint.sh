eeb51ec147282928520669416015f01425033741
saravanpa-aot <Saravan.Azhikoden@gov.bc.ca>
2024-09-19 18:03:09 -0700

    Update docker-entrypoint.sh

--- compliance-api/docker-entrypoint.sh
+++ compliance-api/docker-entrypoint.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
 
+# Vault secrets directory
+VAULT_SECRETS_DIR=/vault/secrets
+
+# Check if the Vault secrets directory exists
+if [ -d "${VAULT_SECRETS_DIR}" ]; then
+  echo "[entrypoint] Vault secrets directory found. Sourcing all files for environment variables."
+
+  set -a  # Automatically export all variables
+  for i in ${VAULT_SECRETS_DIR}/*; do
+    if [ -f "${i}" ]; then
+      echo "[entrypoint] Adding environment variables from ${i}"
+      . "${i}"  # Source each file
+    fi
+  done
+  set +a  # Stop automatically exporting variables
+else
+  echo "[entrypoint] Vault secrets directory (${VAULT_SECRETS_DIR}) does not exist. Proceeding without Vault secrets."
+fi
+
+# Start the application
 echo 'starting application'
-gunicorn --bind 0.0.0.0:8080 --timeout 60 --workers 3  wsgi:application
+exec gunicorn --bind 0.0.0.0:8080 --timeout 60 --workers 3 wsgi:application
