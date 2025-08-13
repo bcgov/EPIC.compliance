7e90dc13b3a4d35b523b08c9c7e7a85b3c948433
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2025-08-13 17:08:00 -0600

    lint fix

--- compliance-api/src/compliance_api/resources/case_file.py
+++ compliance-api/src/compliance_api/resources/case_file.py
@@ -387,5 +387,5 @@ class CaseFileOptions(Resource):
     def get():
         """Fetch active case files as id-name pairs."""
         case_file_options = CaseFileService.get_case_file_options()
-        case_file_option_schema = KeyValueSchema(many=True)
-        return case_file_option_schema.dump(case_file_options), HTTPStatus.OK
+        key_value_schema = KeyValueSchema(many=True)
+        return key_value_schema.dump(case_file_options), HTTPStatus.OK
