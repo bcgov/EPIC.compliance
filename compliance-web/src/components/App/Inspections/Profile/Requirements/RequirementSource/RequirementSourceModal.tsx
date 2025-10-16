09b91572e09f8557ff3a6ce239acf56ae757a280
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2025-10-15 23:28:48 -0600

    COMP-674: Change fields when adding Order as Requirement Source

--- compliance-web/src/components/App/Inspections/Profile/Requirements/RequirementSource/RequirementSourceModal.tsx
+++ compliance-web/src/components/App/Inspections/Profile/Requirements/RequirementSource/RequirementSourceModal.tsx
@@ -277,6 +277,21 @@ const RequirementSourceModal: React.FC<RequirementSourceModalProps> = ({
                   disabled={!!requirementSourceFormData || !!requirementSource}
                   isRequired={true}
                 />
+                {selectedRequirementSource?.id ===
+                  RequirementSourceEnum.ORDER && (
+                  <ControlledAutoComplete
+                    name="order"
+                    label="Order Number"
+                    options={orderList ?? []}
+                    getOptionLabel={(option) => option.order_number ?? ""}
+                    getOptionKey={(option) => option.id ?? ""}
+                    isOptionEqualToValue={(option, value) =>
+                      option.id === value.id
+                    }
+                    disabled={!!requirementSourceFormData || !!order}
+                    isRequired={true}
+                  />
+                )}
                 {selectedRequirementSource && (
                   <Stack direction={"row"} gap={2}>
                     {!isSectionModal && (
@@ -313,21 +328,6 @@ const RequirementSourceModal: React.FC<RequirementSourceModalProps> = ({
                       />
                     )}
                   </Stack>
-                )}
-                {selectedRequirementSource?.id ===
-                  RequirementSourceEnum.ORDER && (
-                  <ControlledAutoComplete
-                    name="order"
-                    label="Order Number"
-                    options={orderList ?? []}
-                    getOptionLabel={(option) => option.order_number ?? ""}
-                    getOptionKey={(option) => option.id ?? ""}
-                    isOptionEqualToValue={(option, value) =>
-                      option.id === value.id
-                    }
-                    disabled={!!requirementSourceFormData || !!order}
-                    isRequired={true}
-                  />
                 )}
                 <ControlledAutoComplete
                   name="appendix"
