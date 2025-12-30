a2fe897c38e2a8f880f7a90548b80a5ba9e6b79a
dinesh <dinesh.pb@aot-technologies.com>
2025-12-30 15:38:16 -0800

    adding relative path for the web interface

--- compliance-web/vite.config.ts
+++ compliance-web/vite.config.ts
@@ -13,6 +13,7 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+  base: process.env.VITE_APP_BASE_PATH || '/',
   plugins: [TanStackRouterVite(), react(), tsconfigPaths(), istanbul({
     cypress: true,
     requireEnv: false
